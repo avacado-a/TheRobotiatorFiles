513b45d539ad9cf3aa108d3ddb62268f523a323b
J3 <136594022+avacado-a@users.noreply.github.com>
2026-07-20 09:55:03 -0400

    Make pin an int

--- db.py
+++ db.py
@@ -3,21 +3,21 @@ import time
 def init_db():
     open("db.txt", "w").close()
 
-def log_action(username: str, pin: str, action: str):
+def log_action(username: str, pin: int, action: str):
     with open("db.txt", "a") as f:
-        f.write(time.strftime("%Y-%m-%d %H:%M:%S") + "|" + username + "|" + pin + "|" + action + "\n")
+        f.write(time.strftime("%Y-%m-%d %H:%M:%S") + "|" + username + "|" + str(pin) + "|" + action + "\n")
 
-def create_user(username: str, pin: str):
+def create_user(username: str, pin: int):
     log_action(username, pin, "create")
 
-def login_user(username: str, pin: str):
+def login_user(username: str, pin: int):
     log_action(username, pin, "login")
 
-def logout_user(username: str, pin: str):
+def logout_user(username: str, pin: int ):
     log_action(username, pin, "logout")
 
-def deactivate_user(username: str, pin: str):
+def deactivate_user(username: str, pin: int):
     log_action(username, pin, "deactivate")
 
-def activate_user(username: str, pin: str):
+def activate_user(username: str, pin: int):
     log_action(username, pin, "activate")
